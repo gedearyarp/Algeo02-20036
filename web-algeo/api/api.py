fb1a2f393dfd6dcde9a85fbabb27708c0e13fb83
rizkyramadhana26 <rizkyramadhana26@gmail.com>
2021-11-10 16:20:16 +0700

    Menambahkan algoritma SVD dan prosedur-prosedur kompresi gambar

--- web-algeo/api/api.py
+++ web-algeo/api/api.py
@@ -1,5 +1,11 @@
 from flask import Flask, request
 from flask_cors import CORS
+from time import clock
+from PIL import Image
+from numpy import array
+from svd import *
+import io
+import base64
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +16,57 @@ def compressImage():
     imageURL = request.get_json()
     imageBase64 = imageURL["data"]
     compressionRates = imageURL["rates"]
-    return imageURL
+    if(compressionRates=='low'):
+        tingkatKompresi=3
+    elif(compressionRates=='med'):
+        tingkatKompresi=2
+    else:
+        tingkatKompresi=1
+    mulai=clock()
+
+    sebelum_img=Image.open(base64.b64decode(imageBase64))
+    sebelum_array=array(sebelum_img)
+    m=sebelum_array.shape[0]
+    n=sebelum_array.shape[1]
+    banyakEigen = min(m,n)
+    k = banyakEigen//(tingkatKompresi*3)
+    if(sebelum_img.mode=='RGB'):
+        setelah_array=kompresiGambarNLayer(sebelum_array,3,k)
+        im = finalisasi(setelah_array)
+        im=base64.b64encode(im)
+    elif(sebelum_img.mode=='L'):
+        setelah_array=kompresiGambarNLayer(sebelum_array,1,k)
+        im = finalisasi(setelah_array)
+        im=base64.b64encode(im)
+    elif(sebelum_img.mode=='RGBA'):
+        a=sebelum_array[:,:,3]
+        setelah_array=kompresiGambarNLayer(sebelum_array,3,k)
+        setelah_array[:,:,3]=a
+        setelah_array=pertahankanTransparansi(setelah_array)
+        im = finalisasi(setelah_array)
+        im=base64.b64encode(im)
+    elif(sebelum_img.mode=='LA'):
+        a=sebelum_array[:,:,1]
+        setelah_array=kompresiGambarNLayer(sebelum_array,1,k)
+        setelah_array[:,:,1]=a
+        setelah_array=pertahankanTransparansi(setelah_array)
+        im = finalisasi(setelah_array)
+        im=base64.b64encode(im)
+    else:
+        try:
+            setelah_array=kompresiGambarNLayer(sebelum_array,sebelum_array.shape[2],k)
+            im = finalisasi(setelah_array)
+            im=base64.b64encode(im)
+        except:
+            print('Proses gagal !')
+            im=''
+
+    selesai=clock()
+    lama=selesai-mulai
+    rasio=100*(k*(m+n)+k)/(m*n)
+    print(f'Waktu pengerjaan : {lama} detik')
+    print(f'Rasio kompresi : {rasio}%')
+    return im,lama,rasio
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ web-algeo/api/svd.py
@@ -0,0 +1,80 @@
+from PIL import Image
+from numpy import array,transpose,matmul,zeros,sqrt,add,dot,diag,uint8,ones,abs,outer
+from numpy.linalg import norm
+
+def eigenDominan(A, toleransi=1e-10):
+    m, n = A.shape
+    k=min(m,n)
+    v = ones(k) / sqrt(k)
+    if m > n:
+        A = matmul(transpose(A),A)
+    elif m < n:
+        A = matmul(A,transpose(A))
+    
+    nilaiEigen = (matmul(A,v)/v)[0]
+
+    while( True):
+        Av = matmul(A,v)
+        vBaru = Av / norm(Av)
+        nilaiEigenBaru = (matmul(A,vBaru)/vBaru)[0]
+        if abs(nilaiEigen - nilaiEigenBaru) < toleransi:
+            break
+
+        v = vBaru
+        nilaiEigen = nilaiEigenBaru
+
+    return nilaiEigenBaru, vBaru
+
+def svd(A, k=None, toleransi=1e-10):
+    A = array(A, dtype=float)
+    m, n = A.shape
+        
+    SVDKeN = []
+    if k is None:
+        k = min(m, n)
+
+    for i in range(k):
+        salinanA = A.copy()
+
+        for u, nilaiSingular, v in SVDKeN[:i]:
+            salinanA -= nilaiSingular * outer(u, v)
+
+        if m > n:
+            _, v = eigenDominan(salinanA, toleransi=toleransi)
+            uAwal = matmul(A,v)
+            sigma = norm(uAwal)  
+            u = uAwal / sigma
+        else:
+            _, u = eigenDominan(salinanA, toleransi=toleransi)
+            vAwal = matmul(transpose(A),u)
+            sigma = norm(vAwal)  
+            v = vAwal / sigma
+
+        SVDKeN.append((u, sigma, v))
+
+    us, S, vs = [array(x) for x in zip(*SVDKeN)]
+    return transpose(us), S, vs
+
+def kompresiSVD(arr,k):
+  u,s,v=svd(arr,k)
+  hasilKompresi=dot(u[:,:k],dot(diag(s[:k]),v[:k,:]))
+  return hasilKompresi
+
+def kompresiGambarNLayer(arr,n,k):
+  hasil=zeros(arr.shape)
+  for i in range(n):
+    hasil[:,:,i]=kompresiSVD(arr[:,:,i],k)
+  return hasil
+
+def pertahankanTransparansi(arr):
+  channel=arr.shape[2]
+  for i in range(arr.shape[0]):
+    for j in range(arr.shape[1]):
+      if arr[i,j,-1]==0:
+        arr[i,j,:]=[0 for x in range(channel)]
+  return arr
+
+def finalisasi(arr):
+  arr=(arr-arr.min())/(arr.max()-arr.min())*255
+  im = Image.fromarray(arr.astype(uint8))
+  return im
