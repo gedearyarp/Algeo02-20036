4d11d24603c6bb42e20eb9e851912c1ad8abbb1f
GedeAryaRP <71829426+gedearyarp@users.noreply.github.com>
2021-11-14 20:50:35 +0700

    Update Copyright.jsx

--- web-algeo/src/misc/Copyright.jsx
+++ web-algeo/src/misc/Copyright.jsx
@@ -3,6 +3,6 @@ import * as React from 'react'
 
 export const Copyright = (props) => (
   <Text fontSize="sm" {...props} color="#01CC90">
-    &copy; {new Date().getFullYear()} Pemuja Hidayah-Mu, diniatkan dengan hati, dikerjakan dengan emosi.
+    &copy; {new Date().getFullYear()} Pemuja Hidayah-Mu, diniatkan dengan hati, dikerjakan dengan hati.
   </Text>
 )
